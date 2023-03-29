0b0c821584fbf497a421aca94100b30b92f728e4
Tatiana SchiBer <tatianaschiavuzzobernardi@gmail.com>
2023-03-29 16:06:35 +0200

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,23 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+	/*	
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 		
 		Seller seller = new Seller(20,"Bob","bob@gmail.com",new Date(),3000.00,obj);
+		SellerDao sellerDao = DaoFactory.creaSellerDao();
+		*/
+		
+		SellerDao sellerDao = DaoFactory.creaSellerDao();
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,12 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
+	
+	public static SellerDao creaSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
 
 }
